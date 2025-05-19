69279d866c43943d08d45130b173b65fe7e8d189
claudineiodt <claudinei.goncalves@optimusdata.com.br>
2025-05-19 15:52:32 -0300

    Adição de header

--- src/app.js
+++ src/app.js
@@ -1,3 +1,7 @@
+// Chatbot da Câmara Municipal de São Paulo
+// (c) 2025 Optimus Data Technology - Uso permitido apenas para estudos
+// Contato: contato@optimusdt.com.br | (11) 98185-5447
+
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
