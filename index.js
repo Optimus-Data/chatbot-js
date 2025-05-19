1e32408dd82c26b23e0e9eed05763bfd91e7539a
Matheus Lopes Ferreira <lopesferreira.matheus9@gmail.com>
2025-05-19 15:53:52 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Chatbot da Câmara Municipal de São Paulo
 // (c) 2025 Optimus Data Technology - Uso permitido apenas para estudos
-// Contato: contato@optimusdt.com.br | (11) 98185-5447
+// Contato: claudinei.goncalves@optimusdata.com.br | (11) 98185-5447
 
 require('./src/app.js');
