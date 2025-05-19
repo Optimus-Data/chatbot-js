29706a94ca644506f78d79eaacf8bb89f14af029
Matheus Lopes Ferreira <lopesferreira.matheus9@gmail.com>
2025-05-19 15:54:04 -0300

    Update routes.js

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,6 +1,6 @@
 // Chatbot da Câmara Municipal de São Paulo
 // (c) 2025 Optimus Data Technology - Uso permitido apenas para estudos
-// Contato: contato@optimusdt.com.br | (11) 98185-5447
+// Contato: claudinei.goncalves@optimusdata.com.br | (11) 98185-5447
 
 const express = require("express");
 const router = express.Router();
